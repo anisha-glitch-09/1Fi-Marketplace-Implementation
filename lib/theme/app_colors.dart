e2de11630938a7675c4a129f0000531cad62b933
anisha-glitch-09 <rajput123anisha@gmail.com>
2026-09-06 21:42:47 +0530

    Add AppColors class for color constants

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const purple = Color(0xFF6C2BD9);
+  static const darkPurple = Color(0xFF3515A8);
+
+  static const background = Color(0xFFF8F8F8);
+  static const card = Colors.white;
+
+  static const textPrimary = Color(0xFF101828);
+  static const textSecondary = Color(0xFF667085);
+
+  static const border = Color(0xFFE5E7EB);
+
+  static const success = Color(0xFF16803C);
+}
