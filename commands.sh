b7beb54c41a8abba477f8ec93a2bec193cdfb09e
anisha-glitch-09 <rajput123anisha@gmail.com>
2026-09-06 21:07:20 +0530

    Add setup instructions for Flutter and Node.js
    Added steps to set up a Flutter app and Node.js backend.

---
+++ commands.sh
@@ -0,0 +1,15 @@
+## Step-1
+mkdir 1fi-marketplace
+cd 1fi-marketplace
+
+## Step-2
+flutter create flutter_app
+
+## Step-3
+mkdir backend
+cd backend
+npm init -y
+
+## Step-4
+npm install express mongoose cors dotenv
+npm install --save-dev nodemon
