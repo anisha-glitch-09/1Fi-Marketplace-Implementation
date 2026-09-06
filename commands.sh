a5551b9a46bc195fddee0d6592ac28d8d9e1459a
anisha-glitch-09 <rajput123anisha@gmail.com>
2026-09-06 21:38:50 +0530

    Update commands.sh with additional steps for setup
    Added instructions for running the seed script and starting the server.

--- commands.sh
+++ commands.sh
@@ -13,3 +13,18 @@ npm init -y
 ## Step-4
 npm install express mongoose cors dotenv
 npm install --save-dev nodemon
+
+## Step-5 After Running seed/products.js run this 4 steps
+## 5.1 Run:
+cd backend
+npm run seed
+## 5.2 Then:
+npm run dev
+## 5.3 You should see:
+MongoDB connected
+Server running on http://localhost:5000
+## 5.4 Test this:
+http://localhost:5000/api/products
+
+## Step-6
+cd flutter_app
